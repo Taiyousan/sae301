ad21d89daa5d6e862304b63f273357a157168cf9
Taiyousan <shamsbenhamou@gmail.com>
2022-11-30 09:03:55 +0100

    ajout crud lieux et user

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,6 +4,8 @@ namespace App\Controller\Admin;
 
 use App\Controller\ManifController;
 use App\Entity\Manifestation;
+use App\Entity\User;
+use App\Entity\Lieux;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -29,5 +31,7 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Les manifestations', 'fas fa-list', Manifestation::class);
+        yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-list', User::class);
+        yield MenuItem::linkToCrud('Lieux', 'fas fa-list', Lieux::class);
     }
 }
