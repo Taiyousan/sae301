ae3b93ff2faffe0656b7c3b76da87888620180d5
unknown <axelbreheret@gmail.com>
2022-12-04 15:42:08 +0100

    rectification git

--- src/Controller/FinalPanierController.php
+++ src/Controller/FinalPanierController.php
@@ -8,16 +8,9 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class FinalPanierController extends AbstractController
 {
-<<<<<<< HEAD
-    #[Route('/final/panier', name: 'app_final_panier')]
-    public function index(): Response
-    {
-
-=======
     #[Route('/final_panier', name: 'app_final_panier')]
     public function index(): Response
     {
->>>>>>> 5e4ccd1519a4a68a0ec62acac68e7ae6c8e57172
         return $this->render('final_panier/index.html.twig', [
             'controller_name' => 'FinalPanierController',
         ]);
